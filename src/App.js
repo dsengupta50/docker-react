d769256d72dc0b26db4cc7693b60659e4a7a01e7
Deb Sengupta <senguptad@vmware.com>
2020-07-30 20:45:25 -0400

    Adding travis yml

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Debabrata Sengupta says you're beautifffulllll!!!
         </p>
         <a
           className="App-link"
