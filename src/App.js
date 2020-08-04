58512bd0a44a0ab65102ac64e7b06b108dc63be4
Deb Sengupta <senguptad@vmware.com>
2020-08-04 19:54:48 -0400

    changed App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Debabrata Sengupta says you're beautifffulllll!!!
+          Debabrata Sengupta says you're just sooooo beautifffulllll!!!
         </p>
         <a
           className="App-link"
